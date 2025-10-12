0daf619618ea65d9c0fc8d55a940c2308050c165
MOISES-DARLAN <moisesdarlan440@gmail.com>
2025-10-12 15:05:36 -0400

    feat: configura a interface de administracao para os modelos

--- ebd/admin.py
+++ ebd/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Aluno, Turma, Chamada
 
-# Register your models here.
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome_completo', 'data_nascimento', 'nome_responsavel', 'data_cadastro')
+    search_fields = ('nome_completo', 'nome_responsavel')
+    list_filter = ('data_nascimento',)
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'professor')
+    search_fields = ('nome',)
+    filter_horizontal = ('alunos',)
+
+@admin.register(Chamada)
+class ChamadaAdmin(admin.ModelAdmin):
+    list_display = ('turma', 'data')
+    list_filter = ('turma', 'data')
+    filter_horizontal = ('alunos_presentes',)
