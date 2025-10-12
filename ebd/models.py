dee572af6056f4957a37dd78b35189989da159c5
MOISES-DARLAN <moisesdarlan440@gmail.com>
2025-10-12 15:03:14 -0400

    feat: cria modelos de dados para Aluno, Turma e Chamada

---
+++ ebd/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Aluno(models.Model):
+    nome_completo = models.CharField(max_length=255)
+    data_nascimento = models.DateField()
+    nome_responsavel = models.CharField(max_length=255, blank=True, null=True)
+    telefone_contato = models.CharField(max_length=20, blank=True, null=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome_completo
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=100)
+    professor = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    alunos = models.ManyToManyField(Aluno, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Chamada(models.Model):
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    data = models.DateField()
+    alunos_presentes = models.ManyToManyField(Aluno, blank=True)
+    observacoes = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('turma', 'data')
+
+    def __str__(self):
+        return f"Chamada da turma '{self.turma.nome}' em {self.data.strftime('%d/%m/%Y')}"
